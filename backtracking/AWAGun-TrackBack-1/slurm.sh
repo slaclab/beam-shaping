05c08737a0da56d129ce538d6e85dfa00088b57c
Neveu <nneveu@hawk.iit.edu>
2022-03-17 22:45:14 -0700

    backtrackign slurm

--- backtracking/AWAGun-TrackBack-1/slurm.sh
+++ backtracking/AWAGun-TrackBack-1/slurm.sh
@@ -11,17 +11,21 @@
 #####SBATCH --mem-per-cpu=1g
 
 #SBATCH --time=00:20:00
+
 # Export SLURM_EXACT because of the new behavior in 21.08
 export SLURM_EXACT=1
 
+# Using my own mpi
 module remove openmpi
 module load devtoolset/9
 module list
 
+# Setting some enviornment variables
 source ~/.bashrc
 export PATH=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/:$PATH
+#export opal=/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/opal
 
-/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/mpiexec -n 9 python call_aposmm.py
+/gpfs/slac/staas/fs1/g/accelerator_modeling/nneveu/software/OPAL/opal_mpich/bin/mpiexec -n 4 opal AWAGun-TrackBack-1.in
 
 #
 # Print the date again -- when finished
