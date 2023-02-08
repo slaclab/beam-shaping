e469875863b610cc0e318655bfb7b16ebe792a0e
nneveu <nneveu@stanford.edu>
2023-02-08 13:08:47 -0800

    starting file ot read elegant files

---
+++ scripts/2023_paper/get_elegant_stats.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+# Load and get parameters from elegant files:
+top_dir         = '/Users/nneveu/github/beam-shaping/sfg'
+dcns_file       = '/joehold/100MeV/elegant_ssnl_100MeV_10mill_de_adjusted_64by512_300emission_steps.txt.out'
+#gauss_flat_file = '/jingyi/end_linac_flat_lht/SXRSTART.out' #SXRSTART_flat_laser.h5 # picture doesn't match?
+gauss_arb_file  = 'jingyi/end_linac_arb_lht/SXRSTART.out' #SXRSTART_arb_laser.h5
