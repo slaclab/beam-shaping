66bf9fd4d43d5ea29b266fc8c533b8d53151418d
nneveu <nneveu@stanford.edu>
2023-03-23 15:53:40 -0700

    getting norm emit for paper plots

--- scripts/2023_paper/get_elegant_stats.py
+++ scripts/2023_paper/get_elegant_stats.py
@@ -2,7 +2,8 @@ import numpy as np
 import h5py
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from pmd_beamphysics import ParticleGroup
+from pmd_beamphysics import ParticleGroup 
+from pmd_beamphysics.statistics import slice_statistics
 from pmd_beamphysics.interfaces.elegant import elegant_h5_to_data
 from pmd_beamphysics.interfaces.opal import opal_to_data
 
@@ -51,9 +52,9 @@ dcns_sxr      = top_dir +'/elegant_files/joehold/tmpa205lw9v/tmpa205lw9v.h5'
 
 
 #For Gauss at end of CM01 - WORKS *with openPMD edits (no ID, no unit check on p)
-#gauss_h5 = h5py.File(gauss_cm01, 'r')
-#gauss_data = elegant_h5_to_data(gauss_h5)
-#h5data     = ParticleGroup(data=gauss_data)
+gauss_h5 = h5py.File(gauss_cm01, 'r')
+gauss_data = elegant_h5_to_data(gauss_h5)
+h5data     = ParticleGroup(data=gauss_data)
 
 #For DCNS at end of CM01 - WORKS
 #dcns_h5 = h5py.File(dcns_cm01, 'r')
@@ -65,28 +66,26 @@ dcns_sxr      = top_dir +'/elegant_files/joehold/tmpa205lw9v/tmpa205lw9v.h5'
 #h5data         = ParticleGroup(data=gauss_sxr_data)
 
 #For DCNS at end of linac - Not working
-dcns_sxr_h5   = h5py.File(dcns_sxr, 'r')
-import pdb; pdb.set_trace()
-dcns_sxr_data = elegant_h5_to_data(dcns_sxr_h5)
-h5data        = ParticleGroup(data=dcns_sxr_data)
+#dcns_sxr_h5   = h5py.File(dcns_sxr, 'r')
+#dcns_sxr_data = elegant_h5_to_data(dcns_sxr_h5)
+#h5data        = ParticleGroup(data=dcns_sxr_data)
 #h5data  = ParticleGroup(h5=dcns_sxr_h5)
 
-
-fig = h5data.slice_plot('norm_emit_x', n_slice=1000) #, slice_key='t')
+#fig = h5data.slice_plot('norm_emit_x', n_slice=1000) #, slice_key='t')
 #plt.title('Gaussian to CM01 end')
 #plt.title('Gaussian to SXR start')
 #plt.title('DCNS to CM01 end')
-plt.title('DCNS to SXR start')
+#plt.title('DCNS to SXR start')
 
 #plt.gca().invert_xaxis()
-plt.show()
+#plt.show()
 #plt.savefig('gauss_norm_emit.pdf',dpi=250)
 #h5data.plot('delta_t', 'delta_pz')
 
-#slice_data = slice_statistics(h5data,  keys=['norm_emit_x'], n_slice=40, slice_key='z')
-#import pdb; pdb.set_trace()
-
-print('sigma_x', h5data['sigma_x'])
-print('energy', h5data['mean_energy'])
-print('gamma',h5data.avg('gamma'))
-print('norm_emit',h5data['norm_emit_x'])
+slice_data = slice_statistics(h5data,  keys=['norm_emit_x'], n_slice=1000, slice_key='t')
+plt.plot(slice_data['norm_emit_x'])
+plt.show()
+#print('sigma_x', h5data['sigma_x'])
+#print('energy', h5data['mean_energy'])
+#print('gamma',h5data.avg('gamma'))
+#print('norm_emit',h5data['norm_emit_x'])
