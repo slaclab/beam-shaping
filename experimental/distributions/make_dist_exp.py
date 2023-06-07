1e9ace14e2d3220be65a2ea6a62291196ce2324c
nneveu <nneveu@stanford.edu>
2023-06-06 23:38:40 -0700

    clean up, making new exp dists

--- experimental/distributions/make_dist_exp.py
+++ experimental/distributions/make_dist_exp.py
@@ -10,11 +10,17 @@ from distgen.writers import *
 from pmd_beamphysics import ParticleGroup
 from pmd_beamphysics.plot import marginal_plot
 
+#2022
 sim_test     = 'tdist_sim.yaml'
 exp_nofilter = 'tdist_exp1_nofilter.yaml'
 exp_p1000fs  = 'tdist_exp2_p1000fs.yaml'
+exp_gauss    = 'tdist_exp_gauss.yaml'
 
-for filename in [sim_test, exp_nofilter,exp_p1000fs]:
+#2023
+exp_raw    = 'tdist_exp_raw.yaml'
+exp_smooth = 'tdist_exp_smooth.yaml'
+
+for filename in [exp_raw, exp_smooth]: #exp_gauss, sim_test, sim_test, exp_nofilter,exp_p1000fs]:
     ext = filename.split('.')[0]
     dist = Generator(input_file=filename, verbose=True)
     dist.run()
